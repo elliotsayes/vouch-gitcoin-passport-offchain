3dc267f900fc9ad003b6d16bea12f62c7ef75412
Elliot Sayes <elliot@sayes.cloud>
2024-08-21 12:58:33 +0800

    Fix

--- server/test/confidence-value.test.js
+++ server/test/confidence-value.test.js
@@ -3,6 +3,6 @@ import * as assert from 'assert'
 import { calculate } from '../lib/calc-confidence-value.js'
 
 test('calculate the correct confidence value for some ao', () => {
-  const value = calculate({ balance: 100_000_000 })
+  const value = calculate({ balance: 100_000_000_000 })
   assert.strictEqual(value, 5)
 });
